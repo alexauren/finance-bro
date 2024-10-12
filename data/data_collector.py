36dd851d42da2d1a1793cacf0043a17064d3b83f
alexauren <alexander.auren@gmail.com>
2024-10-12 17:38:16 +0900

    add api key to data collector

--- data/data_collector.py
+++ data/data_collector.py
@@ -2,12 +2,11 @@ import os
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 
+
 class DataCollector():
 
     def __init__(self):
-        self.api_key = os.getenv('ALPHAVANTAGE_API_KEY')
-        print(self.api_key)
-        # self.api_key = '1MKFCLOIJI3QPW77'
+        self.api_key = '1MKFCLOIJI3QPW77' # Not the actual api key of course, that would be bad practice. I would never do that. 
 
     def fetch(self):
         ts = TimeSeries(self.api_key, output_format='pandas')
