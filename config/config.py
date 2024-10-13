fd5a2fc9efbfbc6b75b206e547837ff0e0b1ed62
alexauren <alexander.auren@gmail.com>
2024-10-13 21:20:30 +0900

    todo. Find out why import fails

--- config/config.py
+++ config/config.py
@@ -1,1 +1,4 @@
 # LSTM
+
+#API
+api_key = "1MKFCLOIJI3QPW77" # Not the actual api key of course, that would be bad practice. I would never do that. 
--- data/data_collector.py
+++ data/data_collector.py
@@ -1,22 +1,22 @@
-import os
-from alpha_vantage.timeseries import TimeSeries
+import config.config as config
 import pandas as pd
-
+from alpha_vantage.timeseries import TimeSeries
 
 class DataCollector():
 
     def __init__(self):
-        self.api_key = '1MKFCLOIJI3QPW77' # Not the actual api key of course, that would be bad practice. I would never do that. 
+        self.api_key = config.api_key
 
-    def fetch(self, stock:str, interval: str, output_format='pandas'):
+    def fetch(self, stock:str, output_format='pandas'):
         ts = TimeSeries(self.api_key, output_format=output_format)
-        data, meta = ts.get_intraday(stock, interval=interval, outputsize='full')
+        data, meta = ts.get_daily(stock, outputsize='compact')
         return (data, meta)
+
     
     def write(self,data, path: str) -> None:
         df = pd.DataFrame(data)
         df.to_csv(f"data/csv/{path}")
 
 dc = DataCollector()
-data, meta = dc.fetch('TSLA', '1min')
+data, meta = dc.fetch('GOOGL')
 dc.write(data, 'testfile')
